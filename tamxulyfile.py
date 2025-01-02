21df2bc4fb693c9625b324b4facbe6d88cfb92e5
huhj225131 <hung20225131@gmail.com>
2025-01-02 21:55:14 +0700

    final

--- tamxulyfile.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-df = pd.read_excel('train_data_raw.xlsx')
-
-sample_data , _ = train_test_split(df, test_size=0.9, random_state=None)
-
-sample_data.to_excel("minitrain4.xlsx", index=False)
-
